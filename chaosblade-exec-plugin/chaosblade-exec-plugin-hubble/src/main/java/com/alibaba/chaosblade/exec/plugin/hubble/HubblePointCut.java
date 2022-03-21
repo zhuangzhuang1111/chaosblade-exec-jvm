92ad9d3556cc87893439fd297e4e8f023e1a6c6f
lizhuangzhuang <lizhuangzhuang@tuhu.cn>
2022-03-21 15:46:27 +0800

    add hubble

--- chaosblade-exec-plugin/chaosblade-exec-plugin-hubble/src/main/java/com/alibaba/chaosblade/exec/plugin/hubble/HubblePointCut.java
+++ chaosblade-exec-plugin/chaosblade-exec-plugin-hubble/src/main/java/com/alibaba/chaosblade/exec/plugin/hubble/HubblePointCut.java
@@ -1,12 +1,13 @@
 package com.alibaba.chaosblade.exec.plugin.hubble;
 
 import com.alibaba.chaosblade.exec.common.aop.PointCut;
-import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.*;
+import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.ClassMatcher;
+import com.alibaba.chaosblade.exec.common.aop.matcher.clazz.SuperClassMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.method.MethodMatcher;
 import com.alibaba.chaosblade.exec.common.aop.matcher.method.NameMethodMatcher;
-import com.alibaba.chaosblade.exec.common.aop.matcher.method.OrMethodMatcher;
 
-import static com.alibaba.chaosblade.exec.plugin.hubble.HubbleConstants.*;
+import static com.alibaba.chaosblade.exec.plugin.hubble.HubbleConstants.HUBBLE_CLASS;
+import static com.alibaba.chaosblade.exec.plugin.hubble.HubbleConstants.HUBBLE_METHOD;
 
 /**
  * @author yefei
